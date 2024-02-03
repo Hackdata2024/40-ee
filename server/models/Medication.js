4a9b87c1c5d495cf32e7b8cc963fcbbd29f2326d
ib565 <ish.bhartiya@gmail.com>
2024-02-03 15:51:53 +0530

    change casing

--- server/models/Medication.js
+++ server/models/Medication.js
@@ -5,11 +5,11 @@ const medicationSchema = new mongoose.Schema({
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   name: { type: String, required: true },
   dosage: { type: String, required: true},
-  reminders_enabled: { type: Boolean, default: true },
-  last_taken: { type: Date, default: null },
-  end_date: { type: Date, default: null },
-  intake_times: { type: [String], match: /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/ , required: true },
-  days_of_week: { type: [{
+  remindersEnabled: { type: Boolean, default: true },
+  lastTaken: { type: Date, default: null },
+  endDate: { type: Date, default: null },
+  intakeTimes: { type: [String], match: /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/ , required: true },
+  daysOfWeek: { type: [{
       type: String,
       enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
     }],
