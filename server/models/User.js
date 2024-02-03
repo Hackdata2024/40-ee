97fd9dee7a9f966f3e72818a4aa464fc98f343ff
ib565 <ish.bhartiya@gmail.com>
2024-02-03 18:07:01 +0530

    remove emergency contact schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,17 @@
 // server/models/User.js
 const mongoose = require('mongoose');
 
-const emergencyContactSchema = new mongoose.Schema({
-    name: { type: String , required: true},
-    phone: { type: String , required: true},
-    relationship: { type: String },
-  });
-
 const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     dateOfBirth: { type: Date, required: true},
     email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
     phoneNumber: { type: String, required: true},
-    emergencyContact: [emergencyContactSchema],
+    emergencyContact: [{
+        name: { type: String , required: true},
+        phone: { type: String , required: true},
+        relationship: { type: String },
+      }],
     password: { type: String, required: true },
 });
 
