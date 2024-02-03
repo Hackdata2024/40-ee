0cf7b716ec194ba937d948badbcff2518500d6cb
NAMAN <ns541@snu.edu.in>
2024-02-04 03:38:10 +0530

    Main Page made

---
+++ client/src/components/Main.js
@@ -0,0 +1,16 @@
+import "./main.css";
+import Home from "./Home";
+import About from "./About";
+import Work from "./Work";
+
+function Main() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Work />
+    </div>
+  );
+}
+
+export default Main;
