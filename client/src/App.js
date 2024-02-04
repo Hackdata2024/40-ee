b6f9d7ec5de61453d56ca532d85b98fafd46e3d9
NAMAN <ns541@snu.edu.in>
2024-02-04 05:45:29 +0530

    Dashboard  working on after being logged in

--- client/src/App.js
+++ client/src/App.js
@@ -2,36 +2,60 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Auth from "./components/Auth/Auth";
-// import Login from './components/Login';
-// import Register from './components/Register';
 import TaskReminders from "./components/TaskReminders/TaskReminders";
 import MedicationAlerts from "./components/MedicationAlerts/MedicationAlerts";
 import EmergencyCalling from "./components/EmergencyCalling/EmergencyCalling";
 import { ThemeProvider } from "./components/Dashboard/ThemeContext";
 import Main from "./components/Main";
+import { SignedOut, SignedIn } from "@clerk/clerk-react";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} /> */}
         <Route path="/" element={<Main />} />
-
+        <Route
+          path="/login"
+          element={
+            <SignedOut>
+              <Auth />
+            </SignedOut>
+          }
+        />
         <Route
           path="/dashboard"
           element={
-            <ThemeProvider>
-              <Dashboard />
-            </ThemeProvider>
+            <SignedIn>
+              <ThemeProvider>
+                <Dashboard />
+              </ThemeProvider>
+            </SignedIn>
           }
         />
-        <Route path="/login" element={<Auth />} />
-        <Route path="/task-reminders" element={<TaskReminders />} />
-
-        <Route path="/medication-alerts" element={<MedicationAlerts />} />
-
-        <Route path="/emergency-calling" element={<EmergencyCalling />} />
+        <Route
+          path="/task-reminders"
+          element={
+            <SignedIn>
+              <TaskReminders />
+            </SignedIn>
+          }
+        />
+        <Route
+          path="/medication-alerts"
+          element={
+            <SignedIn>
+              <MedicationAlerts />
+            </SignedIn>
+          }
+        />
+        <Route
+          path="/emergency-calling"
+          element={
+            <SignedIn>
+              <EmergencyCalling />
+            </SignedIn>
+          }
+        />
       </Routes>
     </Router>
   );
